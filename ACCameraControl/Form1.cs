a5147c2d15fda507830566387829b44ea7962fc6
KagurazakaMiyabi (神楽坂雅詩) <miyabi@uuu.moe>
2025-03-15 20:17:07 +0800

    更新数据时重新排序,更新排序方式,写入文件

--- ACCameraControl/Form1.cs
+++ ACCameraControl/Form1.cs
@@ -1,4 +1,5 @@
 using ACCameraControl.Properties;
+using System.ComponentModel;
 using System.Drawing;
 using System.Security.Cryptography;
 using System.Windows.Forms;
@@ -8,8 +9,13 @@ namespace ACCameraControl
     public partial class Form1 : Form
     {
         private string gameDataOutFile = "dataout.txt";
+        private string saveDataFile = "datain.txt";
         private WindowMove winMv;
         private Control[] cameraButtons;
+        private Int64 sendCounter = 0;
+        private Int64 shotcounter = 0;
+        private int lastSortedColumnIndex = -1;
+        private ListSortDirection lastSortDirection = ListSortDirection.Ascending;
 
         public Form1()
         {
@@ -27,11 +33,13 @@ namespace ACCameraControl
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            Location = new Point(0, 0);
             dataGridView1.Columns[0].ValueType = typeof(int);
             dataGridView1.Columns[0].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.Columns[3].ValueType = typeof(int);
             if (!File.Exists(gameDataOutFile))
             {
-                openFileDialog1.Title = $"找不到文件 {gameDataOutFile} , 请手动指定一个。";
+                openFileDialog1.Title = $"Can't find the file {gameDataOutFile} , please specify one manually.";
                 openFileDialog1.InitialDirectory = Application.StartupPath;
                 if (openFileDialog1.ShowDialog() == DialogResult.OK)
                 {
@@ -62,7 +70,7 @@ namespace ACCameraControl
                 int mustCount = 6;
                 if (valuesCount != mustCount)
                 {
-                    if (MessageBox.Show($"游戏输出数据有错误。\r\n需要数据量: {mustCount}\r\n获得数据量: {valuesCount}", "数据错误", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error) == DialogResult.Cancel)
+                    if (MessageBox.Show($"There is an error in the game output data. \r\n Requires data amount: {mustCount}\r\n Gets data amount: {valuesCount}", "Data Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error) == DialogResult.Cancel)
                     {
                         Application.Exit();
                         return;
@@ -76,9 +84,10 @@ namespace ACCameraControl
                 string dTyres = values[5];
                 dataGridView1.Rows.Add([dPos, dName, dStatus, dID, dSpeed, dTyres]);
             }
-            dataGridView1.Sort(dataGridView1.Columns[0], System.ComponentModel.ListSortDirection.Ascending);
+            dataGridView1.Sort(dataGridView1.Columns[0], ListSortDirection.Ascending);
             dataGridView1.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
             autoResizeGridView();
+            RestoreUserSort();
             //autoResizeForm();
         }
 
@@ -195,12 +204,42 @@ namespace ACCameraControl
             return 0;
         }
 
+        private string nowSelectID()
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+                object cellValue = selectedRow.Cells[3].Value;
+                if (cellValue != null)
+                {
+                    string cellString = (string)cellValue;
+                    return cellString;
+                }
+            }
+            return "0";
+        }
+
+        private void selectIDrow(string selectID)
+        {
+            dataGridView1.ClearSelection();
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.Cells[3].Value != null && row.Cells[3].Value.ToString() == selectID)
+                {
+                    row.Selected = true;
+                    //dataGridView1.FirstDisplayedScrollingRowIndex = row.Index; // 自动滚动到这行
+                    break;
+                }
+            }
+        }
+
         private void clearAllCameras()
         {
             foreach (var btn in cameraButtons)
             {
                 btn.BackColor = Color.Black;
             }
+            shotcounter++;
         }
 
         private void btnCamerasTV_Click(object sender, EventArgs e)
@@ -270,6 +309,58 @@ namespace ACCameraControl
         private void timerRun_Tick(object sender, EventArgs e)
         {
             btnEnableWrite.BackColor = btnEnableWrite.BackColor == Color.Black ? Color.Gray : Color.Black;
+            sendCounter++;
+            string selectID = nowSelectID();
+            // 回报格式是: ID | 镜头号 | 镜头号变一次就+1 | 每两秒递增1
+            string info = $"{selectID}|{nowSelectedCamera()}|{shotcounter}|{sendCounter}";
+            Console.WriteLine(info);
+            try
+            {
+                File.WriteAllText(saveDataFile, info);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            LoadDataToGrid();
+            selectIDrow(selectID);
         }
+
+        private void dataGridView1_SortCompare(object sender, DataGridViewSortCompareEventArgs e)
+        {
+            int index = e.Column.Index;
+            if (index == 0 || index == 3 || index == 4)
+            {
+                if (int.TryParse(e.CellValue1?.ToString(), out int val1) &&
+                    int.TryParse(e.CellValue2?.ToString(), out int val2))
+                {
+                    e.SortResult = val1.CompareTo(val2);
+                    e.Handled = true; // 阻止默认字符串排序
+                }
+            }
+        }
+
+        private void dataGridView1_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                var column = dataGridView1.Columns[e.ColumnIndex];
+                var currentDirection = lastSortDirection == ListSortDirection.Ascending ?
+                                       ListSortDirection.Descending : ListSortDirection.Ascending;
+                dataGridView1.Sort(column, currentDirection);
+                lastSortedColumnIndex = e.ColumnIndex;
+                lastSortDirection = currentDirection;
+            }
+        }
+
+        private void RestoreUserSort()
+        {
+            if (lastSortedColumnIndex >= 0)
+            {
+                var col = dataGridView1.Columns[lastSortedColumnIndex];
+                dataGridView1.Sort(col, lastSortDirection);
+            }
+        }
+
     }
 }
---
+++ ACCameraControl/NumericComparer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACCameraControl
+{
+    class NumericComparer : IComparer
+    {
+        private int columnIndex;
+        private ListSortDirection sortDirection;
+
+        public NumericComparer(int columnIndex, ListSortDirection sortDirection)
+        {
+            this.columnIndex = columnIndex;
+            this.sortDirection = sortDirection;
+        }
+
+        public int Compare(object x, object y)
+        {
+            var row1 = x as DataGridViewRow;
+            var row2 = y as DataGridViewRow;
+
+            double val1 = 0, val2 = 0;
+
+            double.TryParse(row1?.Cells[columnIndex].Value?.ToString(), out val1);
+            double.TryParse(row2?.Cells[columnIndex].Value?.ToString(), out val2);
+
+            int result = val1.CompareTo(val2);
+            return sortDirection == ListSortDirection.Ascending ? result : -result;
+        }
+    }
+}
